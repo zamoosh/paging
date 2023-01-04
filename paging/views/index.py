12dc480a22cdd2eb72be58a00c20d25c9b9417b9
Mohammad Rahimi <moyi.pary@gmail.com>
2023-01-04 20:29:39 +0330

    updates.

--- paging/views/index.py
+++ paging/views/index.py
@@ -6,6 +6,8 @@ class Index(View):
         return render(request, 'index.html')
 
     def post(self, request):
+        context = {}
+
         Process.clear_table()
         Log.clear_table()
 
@@ -13,14 +15,16 @@ class Index(View):
         page_size = int(request.POST.get('page_size'))
         memory_object = Memory.get_memory()
 
-        memory_object.memory = memory
+        memory_object.size = memory
         memory_object.page_count = math.floor(memory / page_size)
         memory_object.left_page = memory_object.page_count
+        memory_object.page_size = page_size
 
         process_array = Process.generate_process()
 
         not_completed = Process.objects.exclude(status__icontains='D').exists()
         time = 0
+        main_log = []
         while not_completed:
             for process in process_array:
                 if process.status == "D":
@@ -41,7 +45,29 @@ class Index(View):
                 time=time,
                 content=Process.get_json_data()
             )
+            time_log = {
+                'time': time,
+                'left_memory': memory_object.left_page * memory_object.page_size,
+                'total_memory': memory_object.size,
+                'used_memory': (memory_object.page_count - memory_object.left_page) * memory_object.page_size
+            }
+            main_log.append(time_log)
+
             time += 1
             not_completed = Process.objects.exclude(status__icontains='D').exists()
+            if not not_completed:
+                memory_object.left_page = memory_object.page_count
+                Log.objects.create(
+                    time=time,
+                    content=Process.get_json_data()
+                )
+                time_log = {
+                    'time': time,
+                    'left_memory': memory_object.left_page * memory_object.page_size,
+                    'total_memory': memory_object.size,
+                    'used_memory': (memory_object.page_count - memory_object.left_page) * memory_object.page_size
+                }
+                main_log.append(time_log)
 
-        return HttpResponse('salam man be to yar ghadimi!!')
+        context['main_log'] = main_log
+        return render(request, 'process_list.html', context)
